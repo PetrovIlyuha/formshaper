c0cc7ddd94c97c7295414e3ed0ec768692a24744
Ilya Petrov <dnamix1@gmail.com>
2021-05-29 11:59:17 +0300

    added shell sidebar

--- src/app/components/screens/main-shell/Shell.jsx
+++ src/app/components/screens/main-shell/Shell.jsx
@@ -1,14 +1,11 @@
 import { Box, Flex } from "@chakra-ui/react";
 import * as React from "react";
-import { useParams } from "react-router";
 import FormDisplay from "../../formCreation/FormDisplay";
 import FormGenerator from "../../formCreation/FormGenerator";
 import { MobileTopBar } from "./MobileTopBar";
 import { Sidebar } from "./Sidebar";
 
 const Shell = () => {
-  const { form, design } = useParams();
-  console.log("form selecteed: ", form, "design sytem: ", design);
   return (
     <Flex h="100vh" flexDirection="column">
       <MobileTopBar />
