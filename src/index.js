e01c63c7b98f52c1a11ad0df453147ded58ad0f1
Ilya Petrov <dnamix1@gmail.com>
2021-05-31 19:05:58 +0300

    adding redux-persist to preserve user progress

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
